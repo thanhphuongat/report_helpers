ad460394ca03015b3671796130ea535332f44859
tien.xuan.vo <tien.xuan.vo@gmail.com>
2017-10-24 09:52:52 +0700

    GO1P-16157 Fix can't export premium courses and fake enrollments

--- Export.php
+++ Export.php
@@ -4,6 +4,7 @@ namespace go1\report_helpers;
 
 use Aws\S3\S3Client;
 use Elasticsearch\Client as ElasticsearchClient;
+use Elasticsearch\Common\Exceptions\Missing404Exception;
 
 class Export
 {
@@ -18,88 +19,98 @@ class Export
         $this->elasticsearchClient = $elasticsearchClient;
     }
 
-    public function doExport($region, $bucket, $key, $fields, $headers, $params, $selectedIds, $excludedIds, $scrollId = null)
+    public function doExport($bucket, $key, $fields, $headers, $params, $selectedIds, $excludedIds, $allSelected, $formatters = [])
     {
-        $tempFile = "/data/{$key}";
-        if (!is_dir(dirname($tempFile))) {
-            mkdir(dirname($tempFile), 0777, true);
-        }
-        $temp = fopen($tempFile, 'a+');
+        $this->s3Client->registerStreamWrapper();
+        $context = stream_context_create(array(
+            's3' => array(
+                'ACL' => 'public-read'
+            )
+        ));
+        // Opening a file in 'w' mode truncates the file automatically.
+        $stream = fopen("s3://{$bucket}/{$key}", 'w', 0, $context);
 
-        if ($selectedIds !== ['All']) {
+        // Write header.
+        fputcsv($stream, $headers);
+
+        if (!$allSelected) {
             // Improve performance by not loading all records then filter out.
-            $params['body']['query']['filtered']['filter']['and'][] = [
-                'terms' => [
-                    'id' => $selectedIds
+            $params['body']['query']['bool']['must'][] = [
+                'ids' => [
+                    'values' => $selectedIds
                 ]
             ];
         }
 
         $params += [
-            'search_type' => 'scan',
             'scroll' => '30s',
-            'size' => 100,
+            'size' => 50,
         ];
 
-        if (!$scrollId) {
-            // Write header.
-            fputcsv($temp, $headers);
-            fclose($temp);
+        $docs = $this->elasticsearchClient->search($params);
+        $scrollId = $docs['_scroll_id'];
 
-            $docs = $this->elasticsearchClient->search($params);
+        while (\true) {
+            if (count($docs['hits']['hits']) > 0) {
+                foreach ($docs['hits']['hits'] as $hit) {
+                    if (empty($excludedIds) || !in_array($hit['_id'], $excludedIds)) {
+                        $csv = $this->getValues($fields, $hit, $formatters);
+                        // Write row.
+                        fputcsv($stream, $csv);
+                    }
+                }
+            }
+            else {
+                if (isset($scrollId)) {
+                    try {
+                        $this->elasticsearchClient->clearScroll([
+                                'scroll_id' => $scrollId,
+                        ]);
+                    }
+                    catch (Missing404Exception $e) {
+                    }
+                }
+                break;
+            }
 
-            return [
-                'scrollId' => $docs['_scroll_id'],
-                'key' => $key,
-            ];
-        }
-        else {
             $docs = $this->elasticsearchClient->scroll([
                 'scroll_id' => $scrollId,
                 'scroll' => '30s',
             ]);
-        }
 
-        if (isset($docs['hits']['hits']) && count($docs['hits']['hits']) > 0) {
-            foreach ($docs['hits']['hits'] as $hit) {
-                if (empty($excludedIds) || !in_array($hit['_source']['id'], $excludedIds)) {
-                    $csv = $this->getValues($fields, $hit);
-                    // Write row.
-                    fputcsv($temp, $csv);
-                }
+            if (isset($docs['_scroll_id'])) {
+                $scrollId = $docs['_scroll_id'];
             }
-            fclose($temp);
+        }
 
-            return [
-                'scrollId' => $docs['_scroll_id'],
-                'key' => $key,
-            ];
-        }
-        else {
-            rewind($temp);
-            $this->s3Client->registerStreamWrapper();
-            $context = stream_context_create(array(
-                's3' => array(
-                    'ACL' => 'public-read'
-                )
-            ));
-            file_put_contents("s3://{$bucket}/{$key}", $temp, 0, $context);
-            unlink($tempFile);
-            return [
-                'file' => "https://s3-{$region}.amazonaws.com/{$bucket}/{$key}"
-            ];
-        }
+        fclose($stream);
     }
 
-    protected function getValues($fields, $hit)
+    public function getFile($region, $bucket, $key)
+    {
+        $domain = getenv('MONOLITH') ? getenv('AWS_S3_ENDPOINT') : "https://s3-{$region}.amazonaws.com";
+        return "$domain/$bucket/$key";
+    }
+
+    protected function getValues($fields, $hit, $formatters = [])
     {
         $values = [];
-        foreach ($fields as $field) {
-            $value = array_get($hit['_source'], $field);
-            if (is_array($value)) {
-                $value = implode(' ', $value);
+        foreach ($fields as $key) {
+            if (isset($formatters[$key]) && is_callable($formatters[$key])) {
+                $values[] = $formatters[$key]($hit);
             }
-            $values[] = $value;
+            else {
+                if (isset($formatters[$key]) && is_string($formatters[$key])) {
+                    $value = array_get($hit['_source'], $formatters[$key]);
+                }
+                else {
+                    $value = $hit['_source'][$key];
+                }
+                if (is_array($value)) {
+                    $value = implode(', ', $value);
+                }
+                $values[] = $value;
+            }
         }
         return $values;
     }
